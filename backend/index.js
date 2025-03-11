46f808cb9117ccf770a1341c9afa6fb26101d4ad
Noah dela Cruz <delacruznoah12898@yahoo.com>
2025-03-11 13:07:03 -0700

    Start route for searching for movies

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,9 @@
 import express from "express";
 import cors from "cors";
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
@@ -7,7 +11,22 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("test");
+    res.send(process.env.TMDB_API_TOKEN);
+});
+
+// GET /movies/search?query={searchTerm} - Search for movies
+//      --url 'https://api.themoviedb.org/3/search/movie?query=pokemon&include_adult=false&language=en-US&page=1' \
+app.get("/api/movies/search", async (req, res) => {
+    let response = await axios.get(
+        `https://api.themoviedb.org/3/search/movie?query=pokemon&include_adult=false&language=en-US`,
+        {
+            headers: {
+                Authorization: `Bearer ${process.env.TMDB_API_TOKEN}`,
+            },
+        }
+    );
+    console.log(response);
+    res.send("/api/movies/search");
 });
 
 const PORT = 8080;

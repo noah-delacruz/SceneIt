48ce9c60d0ea6bf0120e52415009c82eb87b74c1
Noah dela Cruz <delacruznoah12898@yahoo.com>
2025-03-11 16:47:55 -0700

    Create navbar & dark mode toggle

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,36 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../components/Home";
+import "./app.css";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Navbar from "../components/Navbar";
+
 function App() {
-    return <>hello from app.jsx</>;
+    const [darkMode, setDarkMode] = useState(true);
+
+    const toggleDarkMode = () => {
+        console.log("toggleDarkmode clicked");
+        setDarkMode((prevMode) => !prevMode);
+    };
+
+    const darkTheme = createTheme({
+        palette: {
+            mode: darkMode ? "dark" : "light",
+        },
+    });
+
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <BrowserRouter>
+                <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </BrowserRouter>
+        </ThemeProvider>
+    );
 }
 
 export default App;

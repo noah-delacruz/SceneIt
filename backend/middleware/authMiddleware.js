70122755edc4e37785c56a94ca4699a7aeda2cd1
Noah dela Cruz <delacruznoah12898@yahoo.com>
2025-03-13 15:37:28 -0700

    Create auth middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken";
+import User from "../models/userModel.js";
+
+const protect = async (req, res, next) => {
+    let token;
+
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith("Bearer")
+    ) {
+        try {
+            // Get token from header
+            token = req.headers.authorization.split(" ")[1];
+
+            // Verify token
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+            // Get user from token without the password
+            req.user = await User.findById(decoded.id).select("-password");
+
+            next();
+        } catch (error) {
+            console.error("Token verification failed:", error);
+            return res
+                .status(400)
+                .json({ message: "Not authorized, token failed" });
+        }
+    } else {
+        return res.status(400).json({ message: "Not authorized, no token" });
+    }
+};
+
+export default protect;
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from "../models/userModel.js";
+import protect from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 

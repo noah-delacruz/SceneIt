8d47ac6c1776a3a0a9a94551050c034ed0d6381d
Noah dela Cruz <delacruznoah12898@yahoo.com>
2025-03-12 17:46:04 -0700

    Add description to MovieList and truncation

--- frontend/components/MovieCard.jsx
+++ frontend/components/MovieCard.jsx
@@ -6,6 +6,33 @@ import CardActionArea from "@mui/material/CardActionArea";
 
 export default function MovieCard({ movie }) {
     // console.log(movie);
+
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+
+        const months = [
+            "Jan",
+            "Feb",
+            "Mar",
+            "Apr",
+            "May",
+            "Jun",
+            "Jul",
+            "Aug",
+            "Sep",
+            "Oct",
+            "Nov",
+            "Dec",
+        ];
+
+        const month = months[date.getMonth()];
+        const day = date.getDate();
+        const year = date.getFullYear();
+
+        // Return the formatted date string
+        return `${month} ${day}, ${year}`;
+    };
+
     return (
         <>
             <Card sx={{ maxWidth: 350 }}>
@@ -21,10 +48,19 @@ export default function MovieCard({ movie }) {
                             {movie.title}
                         </Typography>
                         <Typography
+                            variant="body4"
+                            sx={{ color: "text.disabled", pb: 2 }}
+                        >
+                            {/* {movie.release_date.split("-")[0]} */}
+                            {formatDate(movie.release_date)}
+                        </Typography>
+                        <Typography
                             variant="body2"
                             sx={{ color: "text.secondary" }}
+                            className="movie-description"
                         >
-                            {movie.release_date.split("-")[0]}
+                            {/* {movie.release_date.split("-")[0]} */}
+                            {movie.overview}
                         </Typography>
                     </CardContent>
                 </CardActionArea>

a6f093d663d84c913eca0c021e4f1a83f224515b
Noah dela Cruz <delacruznoah12898@yahoo.com>
2025-03-13 15:55:18 -0700

    Create routes to get user's favorites and add to user's favorites

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -79,6 +79,59 @@ router.post("/api/users/login", async (req, res) => {
     });
 });
 
+// Get user's favorite movies
+router.get("/api/users/favorites", protect, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.json(user.favorites);
+    } catch (error) {
+        res.status(500).json({ message: "Server error" });
+    }
+});
+
+// Add movie to user's favorites array
+router.post("/api/users/favorites", protect, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id);
+        if (!user) {
+            return res.status(400).json({ message: "User not found" });
+        }
+
+        const { movie } = req.body;
+
+        // Check if given a movie to add
+        if (!movie) {
+            return res.status(400).json({ message: "Movie data missing" });
+        }
+
+        // Check if the movie already exists
+        const isMovieFavorited = user.favorites.find(
+            (currMovie) => currMovie.id === movie.id
+        );
+
+        if (isMovieFavorited) {
+            return res
+                .status(400)
+                .json({ message: "Movie already in favorites" });
+        }
+
+        // Add movie and save changes
+        user.favorites.push(movie);
+        await user.save();
+
+        res.status(201).json({
+            message: "Movie added to favorites",
+            favorites: user.favorites,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server error" });
+    }
+});
+
 // Generate JsonWebToken
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {

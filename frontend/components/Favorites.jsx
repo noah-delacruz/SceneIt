d7ef7de973759c5978c5ae4697ddad280cda647a
Noah dela Cruz <delacruznoah12898@yahoo.com>
2025-03-13 20:14:59 -0700

    Allow for user to view favorites

--- frontend/components/Favorites.jsx
+++ frontend/components/Favorites.jsx
@@ -1,3 +1,51 @@
+import React from "react";
+import axios from "axios";
+import { Box, Typography } from "@mui/material";
+import MovieCard from "./MovieCard";
+
 export default function Favorites() {
-    return "favorites";
+    const token = localStorage.getItem("jwtToken");
+    const [favoriteMovies, setFavoriteMovies] = React.useState([]);
+    const getFavorites = async () => {
+        let response = await axios.get(
+            "http://localhost:8080/api/users/favorites",
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        );
+        console.log(response.data);
+        setFavoriteMovies(response.data);
+    };
+
+    React.useEffect(() => {
+        if (token) {
+            getFavorites();
+        }
+    }, [token]);
+
+    return (
+        <>
+            {favoriteMovies.length === 0 ? (
+                <Typography textAlign="center" variant="h3" sx={{ pt: 1 }}>
+                    No movies favorited
+                </Typography>
+            ) : (
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexWrap: "wrap",
+                        gap: 2,
+                        justifyContent: "center",
+                        padding: 2,
+                    }}
+                >
+                    {favoriteMovies.map((movie) => (
+                        <MovieCard key={movie.id} movie={movie} />
+                    ))}
+                </Box>
+            )}
+        </>
+    );
 }

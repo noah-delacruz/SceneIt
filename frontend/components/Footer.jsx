806142388fa43fa3f91bb417e9a38f2d42bbfb9d
Noah dela Cruz <delacruznoah12898@yahoo.com>
2025-03-14 12:18:59 -0700

    Link to blog post in footer

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -5,6 +5,7 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import ArticleIcon from "@mui/icons-material/Article";
 
 export default function Footer() {
     return (
@@ -30,6 +31,13 @@ export default function Footer() {
                             className="no-link-style"
                         >
                             <LinkedInIcon fontSize="large" />
+                        </a>
+                        <a
+                            href="https://sceneit.hashnode.dev/sceneit-building-a-full-stack-movie-search-application-with-tmdb-api"
+                            target="_blank"
+                            className="no-link-style"
+                        >
+                            <ArticleIcon fontSize="large" />
                         </a>
                     </Typography>
                 </Toolbar>
